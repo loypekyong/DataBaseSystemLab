bb61ea2d289059da86e69f62b5adffa6818b313a
loypekyong <loypekyong@gmail.com>
2024-02-26 11:52:23 +0800

    Iterator changed but still have copied code

--- src/java/simpledb/storage/HeapFileIterator.java
+++ src/java/simpledb/storage/HeapFileIterator.java
@@ -9,87 +9,158 @@ import simpledb.common.Database;
 import simpledb.common.Permissions; 
  
 public class HeapFileIterator implements DbFileIterator { 
- 
     private final TransactionId tid; 
     private final HeapFile heapFile; 
-    private Iterator<Tuple> tupleIterator; 
-    private int currentPageNumber; 
- 
+    private Iterator<Tuple> currentIterator; 
+    private int currentPage;
+
     public HeapFileIterator(TransactionId tid, HeapFile heapFile) { 
         this.tid = tid; 
         this.heapFile = heapFile; 
-        this.tupleIterator = null; 
-        this.currentPageNumber = 0; 
+        this.currentIterator = null; 
+        this.currentPage = -1; 
     } 
- 
-    /* loads the iterator for the first page into the tupleIterator */ 
-    @Override 
-    public void open() throws DbException, TransactionAbortedException { 
-        currentPageNumber = 0; 
-        HeapPageId heapPageId = new HeapPageId(heapFile.getId(), currentPageNumber); 
+
+    @Override
+    public void open() throws DbException, TransactionAbortedException {
+        currentPage = 0;
+        HeapPageId heapPageId = new HeapPageId(heapFile.getId(), currentPage); 
      
-        // Load the first page into the buffer pool 
+        // Load the page into the buffer pool 
         HeapPage heapPage = (HeapPage) Database.getBufferPool().getPage(tid, heapPageId, Permissions.READ_ONLY); 
-     
-        tupleIterator = heapPage.iterator(); 
-    } 
- 
-    /* 
-     * If the current page has more tuples, return true. Otherwise, move to the next page and return true if the next page has tuples. 
-     */ 
-    @Override 
-    public boolean hasNext() throws DbException, TransactionAbortedException { 
-        if (tupleIterator == null) { 
-            return false; // No iterator available 
-        } 
-     
-        // If there is a next tuple in the current page, return true 
-        if (tupleIterator.hasNext()) { 
-            return true; 
-        } 
-     
-        // Move to the next page if available 
-        currentPageNumber++; 
-        if (currentPageNumber < heapFile.numPages()) { 
-            HeapPageId heapPageId = new HeapPageId(heapFile.getId(), currentPageNumber); 
+    
+        currentIterator = heapPage.iterator(); 
+        
+    }
+
+    @Override
+    public boolean hasNext() throws DbException, TransactionAbortedException {
+        if (currentIterator == null) {
+            return false;
+        }
+
+        if (currentIterator.hasNext()) {
+            return true;
+        }
+
+        currentPage++;
+        if (currentPage < BufferPool.numPages) {    
+            HeapPageId heapPageId = new HeapPageId(heapFile.getId(), currentPage); 
      
             // Load the page into the buffer pool 
             HeapPage heapPage = (HeapPage) Database.getBufferPool().getPage(tid, heapPageId, Permissions.READ_ONLY); 
-     
-            tupleIterator = heapPage.iterator(); 
-     
-            return tupleIterator.hasNext(); // Return true if the page has tuples 
-        } 
-     
-        return false; // No more pages with tuples 
-    } 
+        
+            currentIterator = heapPage.iterator(); 
+        
+            return currentIterator.hasNext();
+        }
+
+        return false;
+    }
+
+    @Override
+    public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
+        if (hasNext()) {
+            return currentIterator.next();
+        }
+
+        throw new NoSuchElementException();
+    }
+
+    @Override
+    public void rewind() throws DbException, TransactionAbortedException {
+        close();
+        open();
+    }
+
+    @Override
+    public void close() {
+        currentPage = -1;
+        currentIterator = null;
+    }
+};
+    
  
-    /* 
-     * returns the next tuple into the iteration 
-     */ 
-    @Override 
-    public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException { 
-        if (!hasNext()) { 
-            throw new NoSuchElementException("No more tuples in the file"); 
-        } 
-        return tupleIterator.next(); 
-    } 
+    // private final TransactionId tid; 
+    // private final HeapFile heapFile; 
+    // private Iterator<Tuple> tupleIterator; 
+    // private int currentPageNumber; 
  
-    /* 
-     * close and reopens the iterator 
-     */ 
-    @Override 
-    public void rewind() throws DbException, TransactionAbortedException { 
-        close(); 
-        open(); 
-    } 
+    // public HeapFileIterator(TransactionId tid, HeapFile heapFile) { 
+    //     this.tid = tid; 
+    //     this.heapFile = heapFile; 
+    //     this.tupleIterator = null; 
+    //     this.currentPageNumber = 0; 
+    // } 
  
-    /* 
-     *  
-     */ 
-    @Override 
-    public void close() { 
-        tupleIterator = null; 
-        currentPageNumber = 0; 
-    } 
-}
+    // /* loads the iterator for the first page into the tupleIterator */ 
+    // @Override 
+    // public void open() throws DbException, TransactionAbortedException { 
+    //     currentPageNumber = 0; 
+    //     HeapPageId heapPageId = new HeapPageId(heapFile.getId(), currentPageNumber); 
+     
+    //     // Load the first page into the buffer pool 
+    //     HeapPage heapPage = (HeapPage) Database.getBufferPool().getPage(tid, heapPageId, Permissions.READ_ONLY); 
+     
+    //     tupleIterator = heapPage.iterator(); 
+    // } 
+ 
+    // /* 
+    //  * If the current page has more tuples, return true. Otherwise, move to the next page and return true if the next page has tuples. 
+    //  */ 
+    // @Override 
+    // public boolean hasNext() throws DbException, TransactionAbortedException { 
+    //     if (tupleIterator == null) { 
+    //         return false; // No iterator available 
+    //     } 
+     
+    //     // If there is a next tuple in the current page, return true 
+    //     if (tupleIterator.hasNext()) { 
+    //         return true; 
+    //     } 
+     
+    //     // Move to the next page if available 
+    //     currentPageNumber++; 
+    //     if (currentPageNumber < BufferPool.numPages()) { 
+    //         HeapPageId heapPageId = new HeapPageId(heapFile.getId(), currentPageNumber); 
+     
+    //         // Load the page into the buffer pool 
+    //         HeapPage heapPage = (HeapPage) Database.getBufferPool().getPage(tid, heapPageId, Permissions.READ_ONLY); 
+     
+    //         tupleIterator = heapPage.iterator(); 
+     
+    //         return tupleIterator.hasNext(); // Return true if the page has tuples 
+    //     } 
+     
+    //     return false; // No more pages with tuples 
+    // } 
+ 
+    // /* 
+    //  * returns the next tuple into the iteration 
+    //  */ 
+    // @Override 
+    // public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException { 
+    //     if (!hasNext()) { 
+    //         throw new NoSuchElementException("No more tuples in the file"); 
+    //     } 
+    //     return tupleIterator.next(); 
+    // } 
+ 
+    // /* 
+    //  * close and reopens the iterator 
+    //  */ 
+    // @Override 
+    // public void rewind() throws DbException, TransactionAbortedException { 
+    //     close(); 
+    //     open(); 
+    // } 
+ 
+    // /* 
+    //  *  
+    //  */ 
+    // @Override 
+    // public void close() { 
+    //     tupleIterator = null; 
+    //     currentPageNumber = 0; 
+    // } 
+// }
