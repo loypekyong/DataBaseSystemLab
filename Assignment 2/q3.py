6e235378503bf6481367a97d20bd14737e7b63bc
loypekyong <loypekyong@gmail.com>
2024-04-03 22:09:32 +0800

    q3

--- Assignment 2/q3.py
+++ Assignment 2/q3.py
@@ -11,25 +11,13 @@ spark = SparkSession.builder.appName("Assigment 2 Question 3").getOrCreate()
 # YOUR CODE GOES BELOW
 df = spark.read.option("header", "true").csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
 
-#convert the column "Rating" to float
-df = df.withColumn("Rating", df["Rating"].cast("float"))
+df = df.groupBy("City").avg("Rating").withColumnRenamed("avg(Rating)", "AverageRating")
 
-df = df.groupBy("City").avg("Rating")
+top_df = df.orderBy("AverageRating", ascending=False).limit(3).withColumn("RatingGroup", lit("Top"))
+bot_df = df.orderBy("AverageRating", ascending=True).limit(3).withColumn("RatingGroup", lit("Bot"))
+output = top_df.union(bot_df).orderBy("AverageRating", ascending=False)
+output.show()
 
-# extracts the three cities with the highest and lowest average rating per restaurant
-
-df = df.orderBy("avg(Rating)", ascending=False).limit(3).union(df.orderBy("avg(Rating)", ascending=True).limit(3))
-
-RatingGroup = ["Top", "Top", "Top", "Bottom", "Bottom", "Bottom"]
-
-ratingdf = spark.createDataFrame(RatingGroup, "string").toDF("RatingGroup")
-
-df = df.select["City", "avg(Rating)"].alias("City", "AverageRating")
-
-df = df.withColumn("RatingGroup", ratingdf["RatingGroup"])
-
-df.show()
-
-df.write.option("header", "true").csv("hdfs://%s:9000/assignment2/output/question3" % (hdfs_nn))
+output.write.option("header", "true").csv("hdfs://%s:9000/assignment2/output/question3" % (hdfs_nn))
 
 
