6daef5453a1af98cda80cd280e64814b5769f546
loypekyong <loypekyong@gmail.com>
2024-04-03 18:09:34 +0800

    q1 assignment 2

--- Assignment 2/q1.py
+++ Assignment 2/q1.py
@@ -11,10 +11,8 @@ spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
 
 df = spark.read.option("header", "true").csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
 
-#convert the column "Number of Reviews" to float
-df = df.withColumn("Number of Reviews", df["Number of Reviews"].cast("float"))
 
-df = df.filter(df["Number of Reviews"] == 0.0 or df["Rating"] >= 1.0)
+df = df.filter(((df["Number of Reviews"].isNotNull()) | (df["Rating"] >= 1.0)))
 
 df.show()
 
