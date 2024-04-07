bc1f782bb1f740e93dd9de4e3657d4561c8e0311
loypekyong <loypekyong@gmail.com>
2024-04-07 12:41:39 +0800

    q2

--- Assignment 2/q2.py
+++ Assignment 2/q2.py
@@ -11,12 +11,19 @@ spark = SparkSession.builder.appName("Assigment 2 Question 2").getOrCreate()
 # YOUR CODE GOES BELOW
 df = spark.read.option("header", "true").csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
 
-#ignore rows with null values in the column "Price Range"
 df = df.filter(df["Price Range"].isNotNull())
 
 #group by Price range and see max and min values of Rating
-df = df.groupby("Price Range").agg({"Rating": "max", "Rating": "min"})
+cities_max = df.groupby(["City", "Price Range"]).max("Rating").alias("Rating")
+cities_min = df.groupby(["City", "Price Range"]).min("Rating")
+cities = cities_max.union(cities_min).sort("City")
+cities = cities.withColumnRenamed("max(Rating)", "Rating")
 
-df.show()
+output = df.join(cities, ((cities["City"] == df["City"])& (cities["Rating"] == df["Rating"]) & (cities["Price Range"] == df["Price Range"])), "inner")
 
-df.write.option("header", "true").csv("hdfs://%s:9000/assignment2/output/question2" % (hdfs_nn))
+#if need drop dups:
+output = output.dropDuplicates(subset=["City", "Price Range", "Rating"])
+
+output.show(1000)
+
+output.write.option("header", "true").csv("hdfs://%s:9000/assignment2/output/question2" % (hdfs_nn))
